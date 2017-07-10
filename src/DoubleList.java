5c8f4fc213ac4b4b0ce1b7bf453ee5b305fd18f8
Daniel Eagy <daniel.eagy@gmail.com>
2017-07-10 11:19:27 -0700

    doublelist

--- src/DoubleList.java
+++ src/DoubleList.java
@@ -1,52 +1,121 @@
 
 public class DoubleList<T> implements ListADT<T>{
-
+	private DoubleNode<T> head, tail;
+	private int count;
+	
+	/**
+	 * Initializes the Doublelist. 
+	 */
+	public DoubleList() {
+		head = tail = null;
+		count = 0;
+	}
+	
 	@Override
 	public T removeFirst() {
 		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty())
+			throw new EmptyCollectionException("There are no elements in the collection");
+		else if (head == tail) {
+			DoubleNode<T> temp = tail;
+			head = tail = null;
+			
+			return temp.getElement();
+		}
+		else {
+			DoubleNode<T> temp = tail;
+			temp.setPrev(null);
+			
+			tail = tail.getPrev();
+			
+			return temp.getElement();
+		}
 	}
 
 	@Override
 	public T removeLast() {
 		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty())
+			throw new EmptyCollectionException("There are no elements in the collection");
+		else if (head == tail) {
+			DoubleNode<T> temp = head;
+			head = tail = null;
+			
+			return temp.getElement();
+		}
+		else {
+			DoubleNode<T> temp = head;
+			
+			temp.setNext(null);
+			
+			head = head.getNext();
+			head.setPrev(null);
+			
+			return temp.getElement();
+		}
 	}
 
 	@Override
 	public T remove(T element) {
 		// TODO Auto-generated method stub
-		return null;
+		DoubleNode<T> iterator = head; 
+		
+		while(iterator != null) {
+			if(iterator.getElement().equals(element)) {
+				iterator.getPrev().setNext(iterator.getNext());
+				iterator.getNext().setPrev(iterator.getPrev());
+				
+				return iterator.getElement();
+			}
+			
+			iterator = iterator.getNext();
+		}
+		
+		throw new ElementNotFoundException("There is no such element in this collection.");
 	}
 
 	@Override
 	public T first() {
 		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty())
+			throw new EmptyCollectionException("There are no elements in the collection.");
+		else
+			return tail.getElement();
 	}
 
 	@Override
 	public T last() {
 		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty())
+			throw new EmptyCollectionException("There are no elements in the collection.");
+		else
+			return head.getElement();
 	}
 
 	@Override
 	public boolean contains(T target) {
 		// TODO Auto-generated method stub
+		DoubleNode<T> iterator = head; 
+		
+		while(iterator != null && iterator.getElement().equals(target)) {
+			if(iterator.getElement().equals(target))
+				return true;
+			
+			iterator = iterator.getNext();
+		}
 		return false;
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		return count == 0;
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return count;
 	}
 
 }
